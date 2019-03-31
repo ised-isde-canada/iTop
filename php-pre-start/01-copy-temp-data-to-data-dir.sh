85bfe926632201d062bba18a48e61505758d7d23
Andrew Pitt <apitt@redhat.com>
2019-03-31 10:45:55 -0400

    Updated php-pre-start script.

--- php-pre-start/01-copy-temp-data-to-data-dir.sh
+++ php-pre-start/01-copy-temp-data-to-data-dir.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-cp /tmp/data/* $APP_DATA/data
+echo "Copy initial files into data dir if they don't exist."
+
+if [  -f $APP_DATA/data/index.php ]
+then
+    echo "Data exists, skip copying default data."
+else
+    echo "Data dir empty.  Copy default data."
+    cp -r /tmp/data $APP_DATA
+fi
