c0073e26ad139c27f80f824aa6cbac93ac75d892
ganalex <65351752+ganalex@users.noreply.github.com>
2020-08-24 12:09:37 -0400

    Relabeled Email Enabled Field

--- extensions/AddEmailEnabledField/en.dict.applicationsolution-add-email-enabled-field.php
+++ extensions/AddEmailEnabledField/en.dict.applicationsolution-add-email-enabled-field.php
@@ -7,6 +7,6 @@
  */
 
 Dict::Add('EN US', 'English', 'English', array(
-        'Class:ApplicationSolution/Attribute:APM_List_Present' => 'Present in APM List',
+        'Class:ApplicationSolution/Attribute:APM_List_Present' => 'Email Enabled',
 ));
 ?>
