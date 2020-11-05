6e505056c6457c44407167e1ad3d591840ec033b
ganalex <65351752+ganalex@users.noreply.github.com>
2020-11-05 11:31:25 -0500

    Create en.dict.applicationsolution-add-system-inventory-properties.php

---
+++ extensions/System Inventory Properties/en.dict.applicationsolution-add-system-inventory-properties.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+        'Class:ApplicationSolution/Attribute:Section' => 'Section',
+		'Class:ApplicationSolution/Attribute:Client Groups' => 'Client Groups',
+		'Class:ApplicationSolution/Attribute:ASMD Contact' => 'ASMD Contact',
+		'Class:ApplicationSolution/Attribute:Client Contact' => 'Client Contact',
+		'Class:ApplicationSolution/Attribute:Technology Infrastructure' => 'Technology Infrastructure',
+		'Class:ApplicationSolution/Attribute:Risks and Issues' => 'Risks/Issues',
+		'Class:ApplicationSolution/Attribute:Current Status' => 'Current Status',
+		'Class:ApplicationSolution/Attribute:Documentation' => 'Documentation',
+		'Class:ApplicationSolution/Attribute:Next Steps and Upcoming Plans' => 'Next Steps/Upcoming Plans',
+		'Class:ApplicationSolution/Attribute:Comments' => 'Comments',
+		
+));
+?>
