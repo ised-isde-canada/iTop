1a5888dd3129dc275a24bb49ccf30bcd73c696f1
ganalex <65351752+ganalex@users.noreply.github.com>
2020-11-05 13:41:47 -0500

    Modified Field and Label IDs

--- extensions/SystemInventoryProperties/en.dict.applicationsolution-add-system-inventory-properties.php
+++ extensions/SystemInventoryProperties/en.dict.applicationsolution-add-system-inventory-properties.php
@@ -8,14 +8,14 @@
 
 Dict::Add('EN US', 'English', 'English', array(
         'Class:ApplicationSolution/Attribute:Section' => 'Section',
-		'Class:ApplicationSolution/Attribute:Client Groups' => 'Client Groups',
-		'Class:ApplicationSolution/Attribute:ASMD Contact' => 'ASMD Contact',
-		'Class:ApplicationSolution/Attribute:Client Contact' => 'Client Contact',
-		'Class:ApplicationSolution/Attribute:Technology Infrastructure' => 'Technology Infrastructure',
-		'Class:ApplicationSolution/Attribute:Risks and Issues' => 'Risks/Issues',
-		'Class:ApplicationSolution/Attribute:Current Status' => 'Current Status',
+		'Class:ApplicationSolution/Attribute:Client_Groups' => 'Client Groups',
+		'Class:ApplicationSolution/Attribute:ASMD_Contact' => 'ASMD Contact',
+		'Class:ApplicationSolution/Attribute:Client_Contact' => 'Client Contact',
+		'Class:ApplicationSolution/Attribute:Technology_Infrastructure' => 'Technology Infrastructure',
+		'Class:ApplicationSolution/Attribute:Risks_Issues' => 'Risks/Issues',
+		'Class:ApplicationSolution/Attribute:Current_Status' => 'Current Status',
 		'Class:ApplicationSolution/Attribute:Documentation' => 'Documentation',
-		'Class:ApplicationSolution/Attribute:Next Steps and Upcoming Plans' => 'Next Steps/Upcoming Plans',
+		'Class:ApplicationSolution/Attribute:Next_Step_Upcoming_Plans' => 'Next Steps/Upcoming Plans',
 		'Class:ApplicationSolution/Attribute:Comments' => 'Comments',
 		
 ));
